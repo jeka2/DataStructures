a5348052e438ca4916a2fbc384978ab5a78681fc
Yevgeniy Ivanov <anigeka@gmail.com>
2018-12-08 16:07:57 -0600

    delete method and heap left

--- 06-trees/binary_tree/binary_search_tree.rb
+++ 06-trees/binary_tree/binary_search_tree.rb
@@ -4,7 +4,6 @@ class BinarySearchTree
 
   def initialize(root)
     @root = root
-    @stack = []
   end
 
   
@@ -26,6 +25,9 @@ end
     if root && root.title == data
       return root
     end
+    if !root.left && !root.right 
+      return
+    end
     if root.left
       return find(root.left, data)
     end
@@ -34,7 +36,61 @@ end
     end
   end
 
+  def find_with_parent(parent, root, data)
+    return if !data || !root
+    if root && root.title == data
+      return [parent, root]
+    end
+    if !root.left && !root.right 
+      return
+    end
+    if root.left
+      return find_with_parent(root, root.left, data)
+    end
+    if root.right
+      return find_with_parent(root, root.right, data)
+    end
+  end
+
+  def find_leftmost(node)
+    while node.left 
+      node = node.left
+    end
+    node
+  end
+
+  def find_rightmost(node)
+    while node.right
+      node = node.right
+    end
+    node
+  end
+
+  def replace(parent, node)
+    if !node.left && !node.right
+      if parent.rating > node.rating
+        parent.left = nil
+      else
+        parent.right = nil
+      end
+    elsif node.right
+      replacement = find_leftmost(node.right)
+    elsif node.left 
+      replacement = find_rightmost(node.left)
+    end
+    replacement.left = node.left ? node.left : nil
+    replacement.right = node.right ? node.right : nil
+    if node.rating > parent.rating
+      parent.right = replacement
+    else
+      parent.left = replacement
+    end
+  end
+
   def delete(root, data)
+    return if !root || !data
+    node = find_with_parent(nil, root, data) ##node[0] == parent node[1] == child
+    replace(node[0], node[1]) if node
   end
 
   # Recursive Breadth First Search

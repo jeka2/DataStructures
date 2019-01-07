bb514e233a217b2d8648313dd21f87c164befb02
Yevgeniy Ivanov <anigeka@gmail.com>
2019-01-07 15:10:47 -0600

    m

---
+++ 07-graphs/degrees_spec.rb
@@ -0,0 +1,7 @@
+include RSpec
+
+require_relative 'degrees'
+
+Rspec.describe Degrees type: Class do
+	let (:kevin_bacon) { Node.new }
+end
--- 07-graphs/node.rb
+++ 07-graphs/node.rb
@@ -1,7 +1,11 @@
 class Node
-
 	attr_accessor :name
 	attr_accessor :film_actor_hash
+
+	def initialize(name, film_actor_hash = {})
+		@name = name
+		@film_actor_hash = film_actor_hash
+	end
 
 	def find_kevin_bacon(Node)
 		

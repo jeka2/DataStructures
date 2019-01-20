995766b45f35ef3a5c840c2709ce3611ebac60c7
Yevgeniy Ivanov <anigeka@gmail.com>
2019-01-19 18:26:59 -0600

    exercise complete

--- 07-graphs/degrees_spec.rb
+++ 07-graphs/degrees_spec.rb
@@ -1,7 +1,49 @@
 include RSpec
 
-require_relative 'degrees'
+require_relative 'node'
 
-Rspec.describe Degrees type: Class do
-	let (:kevin_bacon) { Node.new }
+RSpec.describe Find, type: Class do
+	let (:find) { Find.new }
+	let (:kevin_bacon) { Node.new("Kevin Bacon") }
+	let (:james_dean) { Node.new("James Dean") }
+	let (:elizabeth_taylor) { Node.new("Elizabeth Taylor") }
+	let (:chill_willis) { Node.new("Chill Willis") }
+	let (:paul_fix) { Node.new("Paul Fix") }
+	let (:natalie_wood) { Node.new("Natalie Wood") }
+	let (:sal_mineo) { Node.new("Sal Mineo") }
+	let (:ann_doran) { Node.new("Ann Doran") }
+	let (:gregory_peck) { Node.new("Gregory Peck") }
+	let (:john_megna) { Node.new("John Megna") }
+	let (:robert_duvall) { Node.new("Robert Duvall") }
+	let (:larry_semon) { Node.new("Larry Semon") }
+	let (:kate_price) { Node.new("Kate Price") }
+	let (:dorothy_dwan) { Node.new("Dorothy Dwan") }
+	let (:john_pickard) { Node.new("John Pickard") }
+	let (:carlos_rivas) { Node.new("Carlos Rivas") }
+	let (:john_fielder) { Node.new("John Fielder") }
+	let (:john_hurt) { Node.new("John Hurt") }
+	let (:robert_patrick) { Node.new("Robert Patrick") }
+	let (:fourth_layer) { Node.new("Fourth Layer") }
+	let (:fifth_layer) { Node.new("Fifth Layer") }
+	let (:sixth_layer) { Node.new("Sixth Layer") }
+	let (:seventh_layer) { Node.new("Seventh Layer") }
+
+	describe "film_actor_hash" do 
+		it "properly finds a person any number of degrees away with proper error handling" do 
+			##1
+			james_dean.film_actor_hash['Giant'] = [elizabeth_taylor, chill_willis, paul_fix]
+			james_dean.film_actor_hash['Rebel Without a Cause'] = [natalie_wood, sal_mineo, ann_doran]
+			paul_fix.film_actor_hash['To Kill a Mockingbird'] = [gregory_peck, john_megna, robert_duvall]
+			paul_fix.film_actor_hash['The Perfect Clown'] = [larry_semon, kate_price, dorothy_dwan]
+			robert_duvall.film_actor_hash['True Grit'] = [john_pickard, carlos_rivas, john_fielder]
+			robert_duvall.film_actor_hash['Jayne Mansfield\'s Car'] = [john_hurt, robert_patrick, kevin_bacon]
+			robert_patrick.film_actor_hash['Fourth from James Dean'] = [fourth_layer]
+			fourth_layer.film_actor_hash['Fifth from James Dean'] = [fifth_layer]
+			fifth_layer.film_actor_hash['Sixth from James Dean'] = [sixth_layer]
+			sixth_layer.film_actor_hash['Seventh from James Dean'] = [seventh_layer]
+			find.find_kevin_bacon(james_dean)
+		end
+	end
 end
+
+##james dean
--- 07-graphs/node.rb
+++ 07-graphs/node.rb
@@ -2,12 +2,63 @@ class Node
 	attr_accessor :name
 	attr_accessor :film_actor_hash
 
-	def initialize(name, film_actor_hash = {})
+	def initialize(name)
 		@name = name
-		@film_actor_hash = film_actor_hash
+		@film_actor_hash = {}
+		@queue = []
 	end
 
-	def find_kevin_bacon(Node)
-		
+	def []=(key, val)
+		@film_actor_hash[key] = val
+	end
+
+	def [](key)
+		@film_actor_hash[key]
+	end
+
+
+end
+
+class Find
+	def initialize
+		@queue = []
+	end
+
+	def find_kevin_bacon(person, link = [])
+		if @queue != [] ##THE FIRST OBJECT IN THE QUEUE IS DELETED, AS IT'S BEING WORKED ON RIGHT NOW
+			@queue.shift
+		end
+		person.film_actor_hash.each do |movie, stars|
+			stars.each do |star|
+				link_duplicate = link.dup
+				new_link = return_new_link(link_duplicate, movie, person.name)
+				@queue << [star, new_link] unless new_link.length > 5
+				if star.name === "Sixth Layer"
+					return found(new_link)
+				end
+				if @queue == [] 
+					p "Either the name is more than 6 degrees away, or not in the database."
+					return
+				end
+			end
+		end	
+		find_kevin_bacon(@queue[0][0], @queue[0][1]) if @queue
+	end
+
+	def found(link)
+		full_statement = ""
+		link.each do |role|
+			person = role.keys[0].to_s
+			movie = role.values[0]
+			statement = "#{person}, who was in '#{movie}' with "
+			full_statement.concat(statement)
+		end
+		full_statement = full_statement.concat("Kevin Bacon with a total bacon number of #{link.length}.")
+		p full_statement
+	end
+
+	def return_new_link(old_link, movie_name, person_name)
+		new_link = old_link << {"#{person_name}": "#{movie_name}"}
+		return new_link
 	end
 end
